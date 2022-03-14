26bd9bc9ed02f6572f0465a6642a8f3258bd3e9a
ross <ross.yeager@berkeley.edu>
2022-03-14 08:45:03 -0700

    types class for crabs

--- src/crabada/types.py
+++ src/crabada/types.py
@@ -20,15 +20,26 @@ class Faction:
     NO_FACTION = "NO_FACTION"
 
 
+class CrabadaClass:
+    SURGE = 1
+    SUNKEN = 2
+    PRIME = 3
+    BULK = 4
+    CRABOID = 5
+    RUINED = 6
+    GEM = 7
+    ORGANIC = 8
+
+
 CRABADA_ID_TO_CLASS = {
-    1: "SURGE",
-    2: "SUNKEN",
-    3: "PRIME",
-    4: "BULK",
-    5: "CRABOID",
-    6: "RUINED",
-    7: "GEM",
-    8: "ORGANIC",
+    CrabadaClass.SURGE: "SURGE",
+    CrabadaClass.SUNKEN: "SUNKEN",
+    CrabadaClass.PRIME: "PRIME",
+    CrabadaClass.BULK: "BULK",
+    CrabadaClass.CRABOID: "CRABOID",
+    CrabadaClass.RUINED: "RUINED",
+    CrabadaClass.GEM: "GEM",
+    CrabadaClass.ORGANIC: "ORGANIC",
 }
 
 
