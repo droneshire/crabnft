8a6eeb60992b4ff5abb57fe7f6e4b503d02ce55c
ross <ross.yeager@berkeley.edu>
2022-03-26 23:57:46 -0700

    new files for strategies

---
+++ src/crabada/strategies/mining_delayed_reinforce.py
@@ -0,0 +1,96 @@
+import typing as T
+from eth_typing import Address
+
+from crabada.crabada_web2_client import CrabadaWeb2Client
+from crabada.crabada_web3_client import CrabadaWeb3Client
+from crabada.strategies.mining import MiningStrategy
+from crabada.types import IdleGame, Team, TeamMember
+from utils import logger
+from utils.config_types import UserConfig
+from utils.general import get_pretty_seconds
+
+
+class DelayReinforcementStrategy(MiningStrategy):
+    MAX_TIME_REMAINING_DELTA = 60.0 * 3
+    DELAY_BEFORE_REINFORCING = 60.0 * 19
+
+    def __init__(
+        self,
+        address: Address,
+        crabada_w2_client: CrabadaWeb2Client,
+        crabada_w3_client: CrabadaWeb3Client,
+        config: UserConfig,
+    ) -> None:
+        super().__init__(
+            address,
+            crabada_w2_client,
+            crabada_w3_client,
+            config,
+        )
+
+    def should_reinforce(self, mine: IdleGame, verbose=True) -> bool:
+        if not self.crabada_w2.mine_needs_reinforcement(mine):
+            return False
+
+        time_remaining = self.crabada_w2.get_remaining_time_for_action(mine)
+        if time_remaining < self.MAX_TIME_REMAINING_DELTA:
+            return True
+
+        if super()._have_reinforced_at_least_once(mine):
+            return True
+
+        time_since_last_action = self.crabada_w2.get_time_since_last_action(mine)
+        if time_since_last_action > self.DELAY_BEFORE_REINFORCING:
+            return True
+
+        time_till_action_pretty = get_pretty_seconds(
+            int(self.DELAY_BEFORE_REINFORCING - time_since_last_action)
+        )
+        logger.print_warn(
+            f"Mine[{mine['game_id']}]:Not reinforcing for {time_till_action_pretty} due to delay strategy"
+        )
+        return False
+
+
+class PreferOtherMpCrabsAndDelayReinforcement(DelayReinforcementStrategy):
+    def __init__(
+        self,
+        address: Address,
+        crabada_w2_client: CrabadaWeb2Client,
+        crabada_w3_client: CrabadaWeb3Client,
+        config: UserConfig,
+    ) -> None:
+        super().__init__(
+            address,
+            crabada_w2_client,
+            crabada_w3_client,
+            config,
+        )
+
+    def get_reinforcement_crab(
+        self, team: Team, mine: IdleGame, reinforcement_search_backoff: int = 0
+    ) -> T.Optional[TeamMember]:
+        self.reinforcement_search_backoff = reinforcement_search_backoff
+        return super()._get_best_mine_reinforcement(team, mine, use_own_crabs=False)
+
+
+class PreferOwnMpCrabsAndDelayReinforcement(DelayReinforcementStrategy):
+    def __init__(
+        self,
+        address: Address,
+        crabada_w2_client: CrabadaWeb2Client,
+        crabada_w3_client: CrabadaWeb3Client,
+        config: UserConfig,
+    ) -> None:
+        super().__init__(
+            address,
+            crabada_w2_client,
+            crabada_w3_client,
+            config,
+        )
+
+    def get_reinforcement_crab(
+        self, team: Team, mine: IdleGame, reinforcement_search_backoff: int = 0
+    ) -> T.Optional[TeamMember]:
+        self.reinforcement_search_backoff = reinforcement_search_backoff
+        return super()._get_best_mine_reinforcement(team, mine, use_own_crabs=True)
