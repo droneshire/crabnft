7188be076ee63bf6d036ea250e8fdb20be5107cd
ross <ross.yeager@berkeley.edu>
2022-04-17 18:13:42 -0700

    user alias

---
+++ src/utils/user.py
@@ -0,0 +1,16 @@
+from config import ALIAS_POSTFIX
+from utils import logger
+
+
+def get_alias_from_user(user: str) -> str:
+    user_split = user.split(ALIAS_POSTFIX[0])
+
+    if not user_split:
+        return user
+
+    if not user_split[-1].startswith(ALIAS_POSTFIX[1:]):
+        return user
+
+    alias = user_split[0]
+    logger.print_warn(f"mapping {user} -> {alias}")
+    return alias
