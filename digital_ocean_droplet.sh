e837663911f5b317cb4e2f5646bb438a728dbb55
ross <ross.yeager@berkeley.edu>
2022-04-30 19:10:51 -0700

    updated droplet script

--- digital_ocean_droplet.sh
+++ digital_ocean_droplet.sh
@@ -13,17 +13,20 @@ TMP_GITHUB_KEY=/tmp/githubKey
 ssh-keyscan github.com >> $TMP_GITHUB_KEY
 ssh-keygen -lf $TMP_GITHUB_KEY
 echo $TMP_GITHUB_KEY >> ~/.ssh/known_hosts
-ssh-add ~/.ssh/id_ed25519-q
+ssh-add ~/.ssh/id_ed25519
 
 mkdir -p $CRABADA_DIR
 cd $CRABADA_DIR
 
+# add deploy keys
 git clone git@github.com:rossyeager/crabnft.git $REPO_DIR
 
 python3 -m pip install --user virtualenv
 
 cd $REPO_DIR
+python3 -m venv env
 source env/bin/activate
+pip install wheel
 pip install -r requirements.txt
 
 mkdir -p $REPO_DIR/logs/bot
@@ -32,5 +35,5 @@ mkdir -p $REPO_DIR/logs/sniper
 # copy logs dir
 # copy config
 
-tmux new -s mining-bot
+# tmux new -s mining-bot
 cd $REPO_DIR/src
