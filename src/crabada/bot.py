67651776c4b708684676eb0369eeda6563be2b70
ross <ross.yeager@berkeley.edu>
2022-05-06 12:00:25 -0700

    bug

--- src/crabada/bot.py
+++ src/crabada/bot.py
@@ -747,6 +747,10 @@ class CrabadaMineBot:
             if not self.mining_strategy.should_start(team):
                 continue
 
+            # only start one team from group at a time in case there's some staggering
+            # that needs to happen
+            team_group = self.config_mgr.config["mining_teams"].get(team["team_id"], -1)
+
             if self._start_mine(team):
                 groups_started.append(team_group)
 
