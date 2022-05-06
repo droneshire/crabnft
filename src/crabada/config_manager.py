87fed51348e491ae729876e683b4e6454d6398c1
ross <ross.yeager@berkeley.edu>
2022-05-06 00:06:40 -0700

    Revert "wip"
    This reverts commit 9d6c949f30fdbfd99adbcabd9aef6a33c2159da7.

--- src/crabada/config_manager.py
+++ src/crabada/config_manager.py
@@ -71,7 +71,6 @@ class ConfigManager:
 
         logger.print_normal(f"Saving config file to disk")
         config = self._get_save_config()
-        print(config)
         log_dir = logger.get_logging_dir()
         config_file = os.path.join(logger.get_logging_dir(), f"{self.user.lower()}_config.json")
         with open(config_file, "w") as outfile:
