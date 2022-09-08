0a80cb47a3aa25f6bcdaead6cbdd8e3c0103fbe0
ross <ross.yeager@berkeley.edu>
2022-09-08 10:38:39 -0700

    new assets, copy script

---
+++ copy_config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+scp src/config_wyndblast.py  root@bot2:/home/crabada/crabnft/src/
+
+scp src/config_crabada.py  root@bot1:/home/crabada/crabnft/src/
+scp src/config_crabada.py  root@bot2:/home/crabada/crabnft/src/
+scp src/config_crabada.py  root@bot3:/home/crabada/crabnft/src/
---
+++ src/wyndblast/assets.py
@@ -0,0 +1,5 @@
+WYNDBLAST_ASSETS = {
+    "wynd": "https://media.discordapp.net/attachments/932245902859583488/948918187200749588/Avax_Wynd.jpeg?width=300&height=300",
+    "vet": "https://media.discordapp.net/attachments/932245902859583488/947384698387181609/Veteran_Merc_-_Ysia_-_assaulter_-_Water.jpg?width=300&height=300",
+    "hero": "https://media.discordapp.net/attachments/932245902859583488/949523073139351593/hero_sneak_peek.png?width=300&height=300",
+}
