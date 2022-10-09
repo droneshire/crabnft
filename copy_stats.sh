977c41a48c7d2bbe4e425c31df43a39c9cb1fa9d
ross <ross.yeager@berkeley.edu>
2022-10-08 23:18:38 -0700

    update bash scripts

--- copy_stats.sh
+++ copy_stats.sh
@@ -5,3 +5,5 @@ scp root@bot2:/home/crabada/crabnft/logs/crabada/stats/* logs/crabada/stats/
 scp root@bot3:/home/crabada/crabnft/logs/crabada/stats/* logs/crabada/stats/
 
 scp root@bot2:/home/crabada/crabnft/logs/wyndblast/stats/* logs/wyndblast/stats/
+
+scp root@bot2:/home/crabada/crabnft/logs/wyndblast/stats/* logs/pumpskin/stats/
