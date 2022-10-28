e31ab5819f955380dd28cd53dc08b1b998e8b4ab
ross <ross.yeager@berkeley.edu>
2022-10-27 23:26:57 -0700

    avax token client

---
+++ src/web3_utils/avax_web3_client.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import os
+import typing as T
+from eth_typing import Address
+from eth_typing.encoding import HexStr
+from web3.types import TxParams
+
+from utils.price import Avax
+from utils.price import token_to_wei, wei_to_token
+from web3_utils.web3_client import Web3Client
+
+
+class AvaxCWeb3Client(Web3Client):
+    """
+    Client to interact with the AVAX token
+    """
+
+    def get_balance(self) -> Avax:
+        try:
+            balance = self.w3.eth.get_balance(self.user_address)
+        except KeyboardInterrupt:
+            raise
+        except:
+            return 0
+        return wei_to_token(balance)
+
+    def transfer_token(self, to_address: Address, avax: Avax) -> HexStr:
+        tx: TxParams = self.build_transaction_with_value_in_wei(to_address, token_to_wei(avax))
+        return self.sign_and_send_transaction(tx)
