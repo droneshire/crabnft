16ad471c0a3b9bed84978c9cb4f18760765788f0
ross <ross.yeager@berkeley.edu>
2022-11-02 23:01:11 -0700

    common config functions

--- src/utils/config_manager.py
+++ src/utils/config_manager.py
@@ -54,10 +54,47 @@ class ConfigManager:
         raise NotImplementedError
 
     def _load_config(self) -> UserConfig:
-        raise NotImplementedError
+        config_file = self._get_config_file()
+        try:
+            with open(config_file, "r") as infile:
+                byte_key = str.encode(self.encrypt_password)
+                load_config: UserConfig = json.load(infile)
+                copy_config = copy.deepcopy(load_config)
+
+                if load_config["sms_number"] == "+1":
+                    load_config["sms_number"] = ""
+                load_config["private_key"] = decrypt(
+                    byte_key, load_config["private_key"], decode=True
+                ).decode()
+                return load_config
+        except:
+            return copy.deepcopy(self.config)
 
     def _get_empty_new_config(self) -> UserConfig:
-        raise NotImplementedError
+        new_config = copy.deepcopy(self.config)
+
+        assert self.config is not None, "Empty config"
+
+        if "game_specific_configs" not in new_config:
+            new_config["game_specific_configs"] = {}
+
+        delete_keys = []
+
+        for del_key in delete_keys:
+            if del_key in new_config["game_specific_configs"]:
+                del new_config["game_specific_configs"][del_key]
+            if isinstance(self.config["game_specific_configs"][del_key], dict):
+                new_config["game_specific_configs"][del_key] = {}
+            if isinstance(self.config["game_specific_configs"][del_key], bool):
+                new_config["game_specific_configs"][del_key] = False
+            if isinstance(self.config["game_specific_configs"][del_key], int):
+                new_config["game_specific_configs"][del_key] = 0
+            if isinstance(self.config["game_specific_configs"][del_key], float):
+                new_config["game_specific_configs"][del_key] = 0.0
+
+        del new_config["max_gas_price_gwei"]
+        new_config["max_gas_price_gwei"] = 0.0
+        return new_config
 
     def _get_save_config(self) -> T.Dict[T.Any, T.Any]:
         save_config = copy.deepcopy(self.config)
--- src/wyndblast/config_manager_wyndblast.py
+++ src/wyndblast/config_manager_wyndblast.py
@@ -27,48 +27,5 @@ class WyndblastConfigManager(ConfigManager):
         self._print_out_config()
         self._save_config()
 
-    def _load_config(self) -> UserConfig:
-        config_file = self._get_config_file()
-        try:
-            with open(config_file, "r") as infile:
-                byte_key = str.encode(self.encrypt_password)
-                load_config: UserConfig = json.load(infile)
-                copy_config = copy.deepcopy(load_config)
-
-                if load_config["sms_number"] == "+1":
-                    load_config["sms_number"] = ""
-                load_config["private_key"] = decrypt(
-                    byte_key, load_config["private_key"], decode=True
-                ).decode()
-                return load_config
-        except:
-            return copy.deepcopy(self.config)
-
-    def _get_empty_new_config(self) -> UserConfig:
-        new_config = copy.deepcopy(self.config)
-
-        assert self.config is not None, "Empty config"
-
-        if "game_specific_configs" not in new_config:
-            new_config["game_specific_configs"] = {}
-
-        delete_keys = []
-
-        for del_key in delete_keys:
-            if del_key in new_config["game_specific_configs"]:
-                del new_config["game_specific_configs"][del_key]
-            if isinstance(self.config["game_specific_configs"][del_key], dict):
-                new_config["game_specific_configs"][del_key] = {}
-            if isinstance(self.config["game_specific_configs"][del_key], bool):
-                new_config["game_specific_configs"][del_key] = False
-            if isinstance(self.config["game_specific_configs"][del_key], int):
-                new_config["game_specific_configs"][del_key] = 0
-            if isinstance(self.config["game_specific_configs"][del_key], float):
-                new_config["game_specific_configs"][del_key] = 0.0
-
-        del new_config["max_gas_price_gwei"]
-        new_config["max_gas_price_gwei"] = 0.0
-        return new_config
-
     def get_lifetime_stats(self) -> T.Dict[T.Any, T.Any]:
         return copy.deepcopy(NULL_GAME_STATS)
