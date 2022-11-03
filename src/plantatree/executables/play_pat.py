237176fad455c99925f7cfd74da6c70606d50ada
ross <ross.yeager@berkeley.edu>
2022-11-03 00:21:39 -0700

    wip

--- src/plantatree/executables/play_pat.py
+++ src/plantatree/executables/play_pat.py
@@ -20,7 +20,7 @@ from utils.price import get_avax_price_usd
 from utils.security import decrypt_secret
 
 TIME_BETWEEN_PLAYERS = 5.0
-TIME_BETWEEN_RUNS = 30.0
+TIME_BETWEEN_RUNS = 60.0 * 10.0
 
 
 def parse_args() -> argparse.Namespace:
@@ -99,44 +99,44 @@ def harvester() -> None:
     avg_gas_gwei: Average = Average()
     avg_gas_used: Average = Average()
 
-    # try:
-    while True:
-        avax_usd = get_avax_price_usd(IEX_API_TOKEN, args.dry_run)
-        avax_usd_value = avax_usd if avax_usd is not None else 0.0
-        gas_gwei = avg_gas_gwei.get_avg()
-        gas_gwei_value = gas_gwei if gas_gwei else 0
-        logger.print_bold(f"AVAX: ${avax_usd_value:.3f}, Gas: {gas_gwei_value}")
+    try:
+        while True:
+            avax_usd = get_avax_price_usd(IEX_API_TOKEN, args.dry_run)
+            avax_usd_value = avax_usd if avax_usd is not None else 0.0
+            gas_gwei = avg_gas_gwei.get_avg()
+            gas_gwei_value = gas_gwei if gas_gwei else 0
+            logger.print_bold(f"AVAX: ${avax_usd_value:.3f}, Gas: {gas_gwei_value}")
 
+            for bot in bots:
+                bot.run(avax_usd)
+                logger.print_normal(f"Waiting before next user...")
+                avg_gas_gwei.update(bot.avg_gas_gwei.get_avg())
+                avg_gas_used.update(bot.avg_gas_used.get_avg())
+                wait(TIME_BETWEEN_PLAYERS)
+            logger.print_normal(f"Waiting for next round of botting...")
+            wait(TIME_BETWEEN_RUNS)
+    except KeyboardInterrupt:
+        pass
+    except Exception as e:
+        stop_message = f"PAT Alert \U0001F332\n\n"
+        stop_message += f"PAT Bot Stopped \U0000203C\n"
+        if alerts_enabled and TWILIO_CONFIG["enable_admin_sms"]:
+            sms_client = Client(TWILIO_CONFIG["account_sid"], TWILIO_CONFIG["account_auth_token"])
+            message = sms_client.messages.create(
+                body=stop_message,
+                from_=TWILIO_CONFIG["from_sms_number"],
+                to=TWILIO_CONFIG["admin_sms_number"],
+            )
+        if alerts_enabled:
+            stop_message += "Please manually attend your trees until we're back up"
+            try:
+                discord.get_discord_hook("PAT_UPDATES").send(stop_message)
+            except:
+                pass
+        logger.print_fail(traceback.format_exc())
+    finally:
         for bot in bots:
-            bot.run(avax_usd)
-            logger.print_normal(f"Waiting before next user...")
-            avg_gas_gwei.update(bot.avg_gas_gwei.get_avg())
-            avg_gas_used.update(bot.avg_gas_used.get_avg())
-            wait(TIME_BETWEEN_PLAYERS)
-        logger.print_normal(f"Waiting for next round of botting...")
-        wait(TIME_BETWEEN_RUNS)
-    # except KeyboardInterrupt:
-    #     pass
-    # except Exception as e:
-    #     stop_message = f"PAT Alert \U0001F332\n\n"
-    #     stop_message += f"PAT Bot Stopped \U0000203C\n"
-    #     if alerts_enabled and TWILIO_CONFIG["enable_admin_sms"]:
-    #         sms_client = Client(TWILIO_CONFIG["account_sid"], TWILIO_CONFIG["account_auth_token"])
-    #         message = sms_client.messages.create(
-    #             body=stop_message,
-    #             from_=TWILIO_CONFIG["from_sms_number"],
-    #             to=TWILIO_CONFIG["admin_sms_number"],
-    #         )
-    #     if alerts_enabled:
-    #         stop_message += "Please manually attend your trees until we're back up"
-    #         try:
-    #             discord.get_discord_hook("PAT_UPDATES").send(stop_message)
-    #         except:
-    #             pass
-    #     logger.print_fail(traceback.format_exc())
-    # finally:
-    #     for bot in bots:
-    #         bot.end()
+            bot.end()
 
 
 if __name__ == "__main__":
