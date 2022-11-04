2be38b1aae29f042ba5d9fdfd32b1ad9fffa66d9
ross <ross.yeager@berkeley.edu>
2022-11-03 22:17:15 -0700

    wip

--- src/pumpskin/executables/play_pumpskin.py
+++ src/pumpskin/executables/play_pumpskin.py
@@ -23,6 +23,7 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description=__doc__)
     log_dir = logger.get_logging_dir("pumpskin")
     parser.add_argument("--dry-run", action="store_true", help="Dry run")
+    parser.add_argument("--update-config", action="store_true", help="Update config from source")
     parser.add_argument("--quiet", action="store_true", help="Disable alerts")
     parser.add_argument("--log-level", choices=["INFO", "DEBUG", "ERROR", "NONE"], default="INFO")
     parser.add_argument("--log-dir", default=log_dir)
@@ -86,6 +87,7 @@ def run_bot() -> None:
             encrypt_password,
             args.log_dir,
             dry_run=args.dry_run,
+            update_config=args.update_config,
         )
         bot.init()
         bots.append(bot)
