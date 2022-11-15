153865893c1e458c815c4e3d0cde0843dadf5ae3
ross <ross.yeager@berkeley.edu>
2022-11-15 00:11:57 -0800

    wip

--- src/wyndblast/pve.py
+++ src/wyndblast/pve.py
@@ -79,7 +79,7 @@ class PveGame:
     MIN_GAME_DURATION = 10
     MAX_GAME_DURATION = 29
     LEVEL_FIVE_EXP = 500
-    MAX_REPLAYS_PER_CYCLE = 3  # based on a daily reward that is 20x battles per week
+    MAX_REPLAYS_PER_CYCLE = 2  # based on a daily reward that is 20x battles per week
     MIN_CLAIM_CHRO = 5000
     MIN_CHRO_TO_PLAY = 100
 
