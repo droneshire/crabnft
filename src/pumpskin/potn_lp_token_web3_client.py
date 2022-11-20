0a96eb8de04f0530144d37cba7b0047a3eb1d8e0
ross <ross.yeager@berkeley.edu>
2022-11-20 00:46:07 -0800

    potn/ppie lp token clients

---
+++ src/pumpskin/potn_lp_token_web3_client.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import os
+import typing as T
+from eth_typing import Address
+
+from web3_utils.avalanche_c_web3_client import AvalancheCWeb3Client
+from web3_utils.web3_client import Web3Client
+
+
+class PotnLpWeb3Client(AvalancheCWeb3Client):
+    """
+    Interact with the POTN/LP token
+    """
+
+    contract_address = T.cast(Address, "0x06Ca569b7C3053Fa885B37006bFA0eE24a38aECA")
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    # standard erc20 token abi so reusing tus
+    abi_dir = os.path.join(this_dir, "abi", "abi-tus.json")
+    abi = Web3Client._get_contract_abi_from_file(abi_dir)
---
+++ src/pumpskin/ppie_lp_token_web3_client.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import os
+import typing as T
+from eth_typing import Address
+
+from web3_utils.avalanche_c_web3_client import AvalancheCWeb3Client
+from web3_utils.web3_client import Web3Client
+
+
+class PpieLpWeb3Client(AvalancheCWeb3Client):
+    """
+    Interact with the PPIE/LP token
+    """
+
+    contract_address = T.cast(Address, "0x48daf7da11146a81d717682009ce6ee6e6770e58")
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    # standard erc20 token abi so reusing tus
+    abi_dir = os.path.join(this_dir, "abi", "abi-tus.json")
+    abi = Web3Client._get_contract_abi_from_file(abi_dir)
