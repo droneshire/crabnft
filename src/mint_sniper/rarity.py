e1db36e7bcfa62f862e213d660ff15175a09fd32
ross <ross.yeager@berkeley.edu>
2022-12-14 00:49:30 -0800

    probably working nft mint sniper base class

--- src/mint_sniper/rarity.py
+++ src/mint_sniper/rarity.py
@@ -1,24 +1,17 @@
 import os
-import json
 import typing as T
-
-from eth_typing import Address
-from eth_typing.encoding import HexStr
-from web3 import Web3
-from web3.types import TxParams, Wei
 
 from utils import logger
 from utils.discord import DISCORD_WEBHOOK_URL
 from utils.file_util import make_sure_path_exists
 
 
-
-
 class NftCollectionAnalyzerBase:
     ATTRIBUTES_FILE = "attributes.json"
     COLLECTION_FILE = "collection.json"
     RARITY_FILE = "rarity.json"
-    ATTRIBUTES = {}
+    ATTRIBUTES: T.Dict[str, T.Dict[T.Any, T.Any]] = {}
+    MAX_TOTAL_SUPPLY = 0
 
     def __init__(
         self,
@@ -35,49 +28,52 @@ class NftCollectionAnalyzerBase:
         collection_dir = os.path.join(this_dir, "collections", collection_name)
 
         self.files = {
-            "attributes": os.path.join(collection_dir, f"{collection_name}_{ATTRIBUTES_FILE}"),
-            "collection": os.path.join(collection_dir, f"{collection_name}_{COLLECTION_FILE}"),
-            "rarity": os.path.join(collection_dir, f"{collection_name}_{RARITY_FILE}"),
+            "attributes": os.path.join(collection_dir, f"{collection_name}_{self.ATTRIBUTES_FILE}"),
+            "collection": os.path.join(collection_dir, f"{collection_name}_{self.COLLECTION_FILE}"),
+            "rarity": os.path.join(collection_dir, f"{collection_name}_{self.RARITY_FILE}"),
         }
 
         for path in self.files.keys():
             make_sure_path_exists(path)
 
     def get_collection_uri(self) -> str:
+        """
+        Should be overridden by derived class to query the contract for the ipfs-like base URI
+        """
         raise NotImplementedError
 
-    def update_nft_collection_attributes(
-        self,
-        attributes_file: str,
-        pumpskin_collection: str,
-    ) -> T.Dict[int, float]:
-        pumpskin_w2: PumpskinWeb2Client = PumpskinWeb2Client()
-        pumpskins_info = {}
-        pumpskins_stats = copy.deepcopy(self.ATTRIBUTES)
+    def _get_collection_info(self, token_id: int) -> T.Dict[T.Any, T.Any]:
+        info = {}
+        url = self.get_collection_uri()
+        return info
 
-        for pumpskin in range(MAX_TOTAL_SUPPLY):
-            pumpskins_info[pumpskin] = pumpskin_w2.get_pumpskin_info(pumpskin)
-            logger.print_normal(f"Processing pumpskin {pumpskin}...")
-            for attribute in pumpskins_info[pumpskin].get("attributes", []):
-                if attribute["trait_type"] not in pumpskins_stats:
+    def get_nft_collection_attributes(self) -> T.Dict[int, float]:
+        collection_info = {}
+        collection_stats = copy.deepcopy(self.ATTRIBUTES)
+
+        for nft in range(self.MAX_TOTAL_SUPPLY):
+            collection_info[nft] = self.get_collection_info(nft)
+            logger.print_normal(f"Processing nft {nft}...")
+            for attribute in collection_info[nft].get("attributes", []):
+                if attribute["trait_type"] not in collection_stats:
                     logger.print_fail(f"Unknown attribute: {attribute['trait_type']}")
                     continue
                 trait_type = attribute["trait_type"]
                 trait_value = attribute["value"]
-                pumpskins_stats[trait_type][trait_value] = (
-                    pumpskins_stats[trait_type].get(trait_value, 0) + 1
+                collection_stats[trait_type][trait_value] = (
+                    collection_stats[trait_type].get(trait_value, 0) + 1
                 )
 
-        with open(attributes_file, "w") as outfile:
+        with open(self.files["attributes"], "w") as outfile:
             json.dump(
-                pumpskins_stats,
+                collection_stats,
                 outfile,
                 indent=4,
                 sort_keys=True,
             )
-        with open(pumpskin_collection, "w") as outfile:
+        with open(self.files["collection"], "w") as outfile:
             json.dump(
-                pumpskins_info,
+                collection_info,
                 outfile,
                 indent=4,
                 sort_keys=True,
@@ -86,80 +82,92 @@ class NftCollectionAnalyzerBase:
     def calculate_rarity(
         self,
         token_id: int,
-        pumpskin_info: T.Dict[str, T.List[T.Dict[str, str]]],
-        pumpskin_stats: T.Dict[str, T.Any],
-    ) -> Rarity:
-        pumpskin_rarity = {k: 0.0 for k in pumpskin_stats.keys()}
-        pumpskin_traits = {k: 0.0 for k in pumpskin_stats.keys()}
+        nft_info: T.Dict[str, T.List[T.Dict[str, str]]],
+        collection_stats: T.Dict[str, T.Any],
+    ) -> T.Dict[str, T.Dict[T.Any, T.Any]]:
+        """
+        Given the individual nft's attributes and the full collection stats,
+        calculate the attributes rarity of said nft
+        """
+        nft_rarity = {k: 0.0 for k in collection_stats.keys()}
+        nft_traits = {k: 0.0 for k in collection_stats.keys()}
 
-        if "attributes" not in pumpskin_info:
+        if "attributes" not in nft_info:
             return {}
 
-        for attribute in pumpskin_info["attributes"]:
-            pumpskin_traits[attribute["trait_type"]] = attribute["value"]
+        for attribute in nft_info["attributes"]:
+            nft_traits[attribute["trait_type"]] = attribute["value"]
 
         total_trait_count = 0
-        for trait, values in pumpskin_stats.items():
+        for trait, values in collection_stats.items():
             total_count = 0
             pumpkin_trait_count = 0
             for value, count in values.items():
                 total_count += count
-                if value == pumpskin_traits[trait]:
+                if value == nft_traits[trait]:
                     pumpkin_trait_count = count
                     total_trait_count += count
 
             rarity = float(pumpkin_trait_count) / total_count
-            pumpskin_rarity[trait] = {"trait": pumpskin_traits[trait], "rarity": rarity}
+            nft_rarity[trait] = {"trait": nft_traits[trait], "rarity": rarity}
 
-        pumpskin_rarity["Overall"] = {
+        nft_rarity["Overall"] = {
             "trait": None,
-            "rarity": float(total_trait_count) / (total_count * len(pumpskin_stats.keys())),
+            "rarity": float(total_trait_count) / (total_count * len(collection_stats.keys())),
         }
 
-        return pumpskin_rarity
+        return nft_rarity
 
-    def calculate_rarity_from_query(self, token_id: int, attributes_file: str) -> Rarity:
-        pumpskin_w2: PumpskinWeb2Client = PumpskinWeb2Client()
-        pumpskin_info = pumpskin_w2.get_pumpskin_info(token_id)
+    def get_rarity_from_query(self, token_id: int) -> T.Dict[str, T.Dict[T.Any, T.Any]]:
+        """
+        Download info from the web source for a particular nft in the collection
+        and get its attributes rarity
+        """
+        nft_info = self.get_collection_info(token_id)
 
-        with open(attributes_file, "r") as infile:
-            pumpskin_stats = json.load(infile)
+        with open(self.files["attributes"], "r") as infile:
+            collection_stats = json.load(infile)
 
-        return calculate_rarity(token_id, pumpskin_info, pumpskin_stats)
+        return calculate_rarity(token_id, nft_info, collection_stats)
 
-    def calculate_rarity_for_collection(
+    def get_full_collection_rarity(
         self,
-        rarity_file: str = None,
         save_to_disk: bool = False,
     ) -> T.Dict[int, float]:
-        pumpskins_rarity = {}
+        """
+        Use cached collection stats and collection attributes to calculate rarity
+        for the entire collection and optionally save that to disk.
 
-        attributes_file = get_json_path(ATTRIBUTES_FILE)
-        with open(attributes_file, "r") as infile:
-            pumpskin_stats = json.load(infile)
+        Returns the cached rarity map for the entire collection
+        """
+        collection_rarity = {}
 
-        collections_file = get_json_path(COLLECTION_FILE)
-        with open(collections_file, "r") as infile:
+        assert os.path.isfile(self.files["attributes"]), "Missing attribute file!"
+        with open(self.files["attributes"], "r") as infile:
+            collection_stats = json.load(infile)
+
+        assert os.path.isfile(self.files["attributes"]), "Missing collection file!"
+        with open(self.files["collection"], "r") as infile:
             collection_traits = json.load(infile)
 
-        for pumpskin in range(MAX_TOTAL_SUPPLY):
-            pumpskins_rarity[pumpskin] = calculate_rarity(
-                pumpskin, collection_traits[str(pumpskin)], pumpskin_stats
+        for nft in range(self.MAX_TOTAL_SUPPLY):
+            collection_rarity[nft] = calculate_rarity(
+                nft, collection_traits[str(nft)], collection_stats
             )
 
-        sorted_pumpskins_rarity = dict(
+        sorted_collection_rarity = dict(
             sorted(
-                pumpskins_rarity.items(),
+                collection_rarity.items(),
                 key=lambda y: y[1].get("Overall", {"rarity": 1000.0})["rarity"],
             )
         )
 
-        if save_to_disk and rarity_file is not None:
-            with open(rarity_file, "w") as outfile:
+        if save_to_disk and self.files["rarity"] is not None:
+            with open(self.files["rarity"], "w") as outfile:
                 json.dump(
-                    sorted_pumpskins_rarity,
+                    sorted_collection_rarity,
                     outfile,
                     indent=4,
                 )
 
-        return sorted_pumpskins_rarity
+        return sorted_collection_rarity
