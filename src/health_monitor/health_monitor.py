1833d0727ab96e117c2547a0f143933fca902efe
Ross Yeager <ryeager12@gmail.com>
2022-12-30 00:07:29 -0800

    health monitor

---
+++ src/health_monitor/health_monitor.py
@@ -0,0 +1,63 @@
+import json
+import requests
+import threading
+
+from utils import logger
+from utils.config_types import UserConfig
+from utils.user import get_alias_from_user
+
+
+class HealthMonitor:
+    SLEEP_TIME = 60.0 * 2.0
+
+    def __init__(
+        self,
+        server_url: str,
+        bot_name: str,
+        users: T.List[UserConfig],
+        verbose: bool = False,
+    ) -> None:
+        self.bot_name = bot_name
+        self.users = users
+        self.server_url = server_url
+        self.verbose = verbose
+
+    def update(self) -> None:
+        url = self.server_url + f"/health/{self.bot_name}"
+
+        aliases = {}
+        for user in self.users:
+            alias = get_alias_from_user(user)
+            alias[alias] = alias.get(alias, 0) + 1
+
+        users = []
+        for user, wallets in aliases.items():
+            users.append({"username": user, "wallets": wallets})
+
+        data = {
+            "name": self.bot_name,
+            "num_users": len(self.users),
+            "users": users,
+        }
+
+        if self.verbose:
+            logger.print_normal(f"Ping from {self.bot_name}...")
+            logger.print_normal(f"{json.dumps(data,indent=4)}")
+
+        try:
+            response = requests.post(
+                url,
+                json=json.loads(json.dumps(data)),
+                headers={"accept": "application/json, text/plain, */*"},
+            ).json()
+        except:
+            logger.print_fail(f"Failed to update {self.server_url} for {self.bot_name}")
+
+    def run(self) -> None:
+        def loop() -> None:
+            while True:
+                self.update()
+                time.sleep(self.SLEEP_TIME)
+
+        thread = threading.Thread(name=f"{self.bot_name}_health_monitor", target=loop, daemon=True)
+        thread.start()
