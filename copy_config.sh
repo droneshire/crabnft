95bd528b50cf250bde55a47a2101ed371fbbc0cc
Ross Yeager <ryeager12@gmail.com>
2022-12-30 18:53:18 -0800

    wip

--- copy_config.sh
+++ copy_config.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 scp src/config_wyndblast.py  root@bot:/home/crabada/crabnft/src/
-
-# scp src/config_crabada.py  root@bot1:/home/crabada/crabnft/src/
-# scp src/config_crabada.py  root@bot:/home/crabada/crabnft/src/
-# scp src/config_crabada.py  root@bot3:/home/crabada/crabnft/src/
-
 scp src/config_pumpskin.py  root@bot:/home/crabada/crabnft/src/
-
 scp src/config_pat.py  root@bot:/home/crabada/crabnft/src/
-
 scp src/config_admin.py  root@bot:/home/crabada/crabnft/src/
+
+# scp root@bot:/home/crabada/crabnft/src/config_wyndblast.py src/config_wyndblast.py
+# scp root@bot:/home/crabada/crabnft/src/config_pumpskin.py src/config_pumpskin.py
+# scp root@bot:/home/crabada/crabnft/src/config_pat.py src/config_pat.py
+# scp root@bot:/home/crabada/crabnft/src/config_admin.py src/config_admin.py
