9b242aaaf2b792341dd48057ecdfbeda315cd16b
Ross Yeager <ryeager12@gmail.com>
2022-12-31 18:07:24 -0800

    wip

--- copy_stats.sh
+++ copy_stats.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# scp root@bot1:/home/crabada/crabnft/logs/crabada/stats/* logs/crabada/stats/
-# scp root@bot:/home/crabada/crabnft/logs/crabada/stats/* logs/crabada/stats/
-# scp root@bot3:/home/crabada/crabnft/logs/crabada/stats/* logs/crabada/stats/
-
 scp root@bot:/home/crabada/crabnft/logs/wyndblast/stats/* logs/wyndblast/stats/
-
 scp root@bot:/home/crabada/crabnft/logs/pumpskin/stats/* logs/pumpskin/stats/
+scp root@bot:/home/crabada/crabnft/logs/pat/stats/* logs/pat/stats/
+
+# scp  logs/wyndblast/stats/* root@bot:/home/crabada/crabnft/logs/wyndblast/stats/
+# scp  logs/pumpskin/stats/* root@bot:/home/crabada/crabnft/logs/pumpskin/stats/
+# scp  logs/pat/stats/* root@bot:/home/crabada/crabnft/logs/pat/stats/
