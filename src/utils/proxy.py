5ad048eddd7e96f5d69e49e386761cbdcd915132
Ross Yeager <ryeager12@gmail.com>
2022-12-31 23:14:56 -0800

    wip

---
+++ src/utils/proxy.py
@@ -0,0 +1,35 @@
+import random
+import requests
+
+from utils import logger
+
+GET_PROXY_LIST = "https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all"
+
+
+class Proxies:
+    def __init__(self) -> None:
+        self.proxies = []
+
+    def init(self) -> None:
+        params = {
+            "request": "getproxies",
+            "protocol": "http",
+            "timeout": 10000,
+            "country": "all",
+            "ssl": "all",
+            "anonymity": "all",
+        }
+        try:
+            response = requests.get(GET_PROXY_LIST).text()
+            self.proxies = response.split()
+            logger.print_ok_arrow(f"Found {len(self.proxies)} proxies!")
+        except:
+            logger.print_fail(f"Failed to get proxies")
+
+    def get_proxy(self) -> str:
+        if not self.proxies:
+            logger.print_fail(f"No proxies available!")
+            return ""
+
+        index = random.randbytes(len(self.proxies))
+        return self.proxies[index]
