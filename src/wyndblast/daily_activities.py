7b7011481a836f6ad57e75a01737d6d6bc83f23f
Ross Yeager <ryeager12@gmail.com>
2023-01-12 22:29:20 -0800

    wip

--- src/wyndblast/daily_activities.py
+++ src/wyndblast/daily_activities.py
@@ -344,13 +344,13 @@ class DailyActivitiesGame:
             if rewards["elemental_stones"] is not None:
                 with self.stats.daily() as da:
                     self.current_stats["elemental_stones"][rewards["elemental_stones"]] += 1
-                    setattr(
-                        da.elemental_stones,
-                        rewards["elemental_stones"],
-                        da.elemental_stones,
-                        rewards["elemental_stones"] + 1,
-                    )
                     self.current_stats["elemental_stones"]["elemental_stones_qty"] += 1
+                    previous_value = getattr(
+                        da.elemental_stones, rewards["elemental_stones"].lower()
+                    )
+                    setattr(
+                        da.elemental_stones, rewards["elemental_stones"].lower(), previous_value + 1
+                    )
 
         outcome_emoji = "\U0001F389" if did_succeed else "\U0001F915"
 
