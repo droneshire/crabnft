97440402a000d709045c3a822ea7cc36f82fdc70
Ross Yeager <ryeager12@gmail.com>
2023-01-29 20:58:59 -0800

    wip

--- copy_stats.sh
+++ copy_stats.sh
@@ -4,6 +4,11 @@ scp root@bot:/home/crabada/crabnft/logs/wyndblast/stats/* logs/wyndblast/stats/
 scp root@bot:/home/crabada/crabnft/logs/pumpskin/stats/* logs/pumpskin/stats/
 scp root@bot:/home/crabada/crabnft/logs/pat/stats/* logs/pat/stats/
 
+scp root@bot:/home/crabada/crabnft/logs/wyndblast/database/* logs/wyndblast/database/
+scp root@bot:/home/crabada/crabnft/logs/pumpskin/database/* logs/pumpskin/database/
+scp root@bot:/home/crabada/crabnft/logs/pat/database/* logs/pat/database/
+scp root@bot:/home/crabada/crabnft/logs/p2e/database/* logs/p2e/database/
+
 # scp  logs/wyndblast/stats/* root@bot:/home/crabada/crabnft/logs/wyndblast/stats/
 # scp  logs/pumpskin/stats/* root@bot:/home/crabada/crabnft/logs/pumpskin/stats/
 # scp  logs/pat/stats/* root@bot:/home/crabada/crabnft/logs/pat/stats/
