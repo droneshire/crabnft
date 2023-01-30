690819eadef62b282bcb762a18f7f7e4f343285c
Ross Yeager <ryeager12@gmail.com>
2023-01-30 14:43:09 -0800

    wip

--- src/mechavax/executables/monitor_mech_mints.py
+++ src/mechavax/executables/monitor_mech_mints.py
@@ -9,7 +9,17 @@ from utils import logger
 
 
 GUILD_WALLET_ADDRESS = "0xA3270d8bF65039680cdC9f61f83578c85ca9ad47"
-STATS_INTERVAL = 60.0 * 5.0
+STATS_INTERVAL = 60.0 * 60.0
+
+GUILD_WALLET_MAPPING = {
+    "0xB45ef8Aec53d763E7B18a01f1C43BE2825Bd3b36": "Star Fox",
+    "0x9B258494A49837dFE0d8874170dB82FE1e638552": "Off Regularly",
+    "0x5Bf93365bbf06DDE2669B2633E50fCFb3f6f8e1C": "Blue Digger",
+    "0xD5f769CbC89773775792Def99ece766Ed65BC2c2": "Almugu",
+    "0xBe8CFD634Ab8D91BA391D3A22D3E1D452c0A43cb": "Nft Cashflow",
+    "0xf17668Ff22c63E63475D5f7DbDf7585D51E52c76": "Primata",
+    "0x26930eb38E475ba24F36d6403271927b95836caa": "Secret Smoothies",
+}
 
 
 def parse_args() -> argparse.Namespace:
@@ -29,7 +39,7 @@ def run_bot() -> None:
     log_dir = os.path.join(args.log_dir, "wyndblast")
     logger.setup_log(args.log_level, log_dir, "mechavax_monitor")
 
-    monitor = MechMonitor(args.address, "MECHAVAX_BOT", STATS_INTERVAL)
+    monitor = MechMonitor(args.address, GUILD_WALLET_MAPPING, "MECHAVAX_BOT", STATS_INTERVAL)
     monitor.run()
 
 
--- src/mechavax/monitor.py
+++ src/mechavax/monitor.py
@@ -1,24 +1,72 @@
 import asyncio
+import json
+import requests
+import web3
+import typing as T
 
 from eth_typing import Address
 from web3 import Web3
-
 from config_admin import ADMIN_ADDRESS
 from mechavax.mechavax_web3client import MechContractWeb3Client, MechArmContractWeb3Client
 from utils import discord, logger
 from utils.price import wei_to_token, TokenWei
+from utils.web2_client import Web2Client
 from web3_utils.avalanche_c_web3_client import AvalancheCWeb3Client
+
+
+class SnowtraceApi(Web2Client):
+    SNOWTRACE_API_URL = "https://api.snowtrace.io/api"
+
+    def __init__(self) -> None:
+        super().__init__(self.SNOWTRACE_API_URL)
+
+    async def get_erc721_token_transfers(
+        self, target_address: Address
+    ) -> T.Dict[Address, T.Dict[str, T.List[int]]]:
+        url = self.SNOWTRACE_API_URL
+        params = {
+            "module": "account",
+            "action": "tokennfttx",
+            "address": target_address,
+            "startblock": 0,
+            "endblock": 999999999,
+            "sort": "asc",
+        }
+        tokens = {}
+        # try:
+        response = self._get_request(url, params=params)
+        for token in response["result"]:
+            address = token["from"]
+            token_id = token["tokenID"]
+            nft_type = token["tokenSymbol"]
+            if address not in tokens:
+                tokens[address] = {}
+            tokens[address][nft_type] = tokens[address].get(nft_type, []) + [token_id]
+            logger.print_normal(f"Found {nft_type} {token_id} from {address}")
+        # except:
+        #     logger.print_fail(f"Failed to get token transfers")
+
+        return tokens
 
 
 class MechMonitor:
     MONITOR_INTERVAL = 5.0
 
-    def __init__(self, address: Address, discord_channel: str, interval: float) -> None:
+    def __init__(
+        self,
+        address: Address,
+        address_mapping: T.Dict[Address, str],
+        discord_channel: str,
+        interval: float,
+    ) -> None:
         self.interval = interval
         self.webhook = discord.get_discord_hook(discord_channel)
         self.address = address
+        self.address_mapping = address_mapping
 
-        self.w3_mech_mech: MechContractWeb3Client = (
+        self.snowtrace_api = SnowtraceApi()
+
+        self.w3_mech: MechContractWeb3Client = (
             MechContractWeb3Client()
             .set_credentials(ADMIN_ADDRESS, "")
             .set_node_uri(AvalancheCWeb3Client.NODE_URL)
@@ -34,45 +82,67 @@ class MechMonitor:
         )
 
         self.event_filters: T.Dict[web3._utils.filters.LogFilter, T.Callable[[T.Any], None]] = {
-            self.w3_mech.contract.events.MechPurchased.createFilter(
+            self.w3_mech.contract.events.LegendaryMechMinted.createFilter(
                 fromBlock="latest"
-            ): self.mint_handler,
+            ): self.legendary_minted_handler,
             self.w3_mech.contract.events.ShirakBalanceUpdated.createFilter(
                 fromBlock="latest"
             ): self.shirak_mint_handler,
+            self.w3_mech.contract.events.ArmsBonded.createFilter(
+                fromBlock="latest"
+            ): self.arms_bonded_handler,
         }
 
-    def mint_handler(self, event) -> None:
-        event_data = Web3.toJSON(event)
+    def arms_bonded_handler(self, event: web3.datastructures.AttributeDict) -> None:
+        event_data = json.loads(Web3.toJSON(event))
         try:
-            tx_hash = event_data["transactionHash"]
-            tx_receipt = self.w3_mech.get_transaction_receipt(tx_hash)
-            transaction = tx_reciept["logs"][1]
-            price_wei = int(transaction["data"], 16)
-            price = wei_to_token(price_wei)
-            mint_item = "MARM" if transaction["address"] == w3_arm.contract_address else "MECH"
+            user = event_data["args"]["user"]
+            token_id = event_data["args"]["mechId"]
+            nft = event_data["args"]["nft"]
+            arm_id = event_data["args"]["id"]
         except:
-            logger.print_warn(f"Failed to process shirak mint event")
+            logger.print_warn(f"Failed to process legendary minted event")
             return
 
-        logger.print_ok_blue(f"Shirak {mint_item} mint event!\nPrice paid: {price:.2f} $SHK")
-        self.webhook.send(f"\U0001F916 New {mint_item} Alert!\n\tMinted Price: `{price:.2f} $SHK`")
+        logger.print_ok_blue(
+            f"\U0001F916 Arms bonded event: `{user}`, ID: `{token_id}`\nNFT: `{nft}` ID: `{arm_id}`"
+        )
+        self.webhook.send(
+            f"\U0001F916 Arms bonded event: `{user}`, ID: `{token_id}`\nNFT: `{nft}` ID: `{arm_id}`"
+        )
 
-    def shirak_mint_handler(self, event) -> None:
-        event_data = Web3.toJSON(event)
+    def legendary_minted_handler(self, event: web3.datastructures.AttributeDict) -> None:
+        event_data = json.loads(Web3.toJSON(event))
         try:
-            tx_hash = event_data["transactionHash"]
-            tx_receipt = self.w3_mech.get_transaction_receipt(tx_hash)
-            transaction = tx_reciept["logs"][1]
-            price_wei = int(transaction["data"], 16)
-            price = wei_to_token(price_wei)
-            mint_item = "MARM" if transaction["address"] == w3_arm.contract_address else "MECH"
+            user = event_data["args"]["user"]
+            token_id = event_data["args"]["mechId"]
+            burned_mechs = event_data["args"]["burnedMechIds"]
         except:
-            logger.print_warn(f"Failed to process shirak mint event")
+            logger.print_warn(f"Failed to process legendary minted event")
             return
 
+        logger.print_ok_blue(f"New ascended minted: {user}, {token_id}, {burned_mechs}")
+        self.webhook.send(
+            f"\U0001F916 New ascended minted: `{user}`, ID: `{token_id}`\nBurned mechs: {burned_mechs}"
+        )
+
+    def shirak_mint_handler(self, event: web3.datastructures.AttributeDict) -> None:
+        event_data = json.loads(Web3.toJSON(event))
+        # try:
+        tx_hash = event_data["transactionHash"]
+        tx_receipt = self.w3_mech.get_transaction_receipt(tx_hash)
+        transaction = tx_receipt["logs"][1]
+        price_wei = int(transaction["data"], 16)
+        price = wei_to_token(price_wei)
+        mint_item = "MARM" if transaction["address"] == self.w3_arm.contract_address else "MECH"
+        # except:
+        #     logger.print_warn(f"Failed to process shirak mint event\n{event_data}")
+        #     return
+
         logger.print_ok_blue(f"Shirak {mint_item} mint event!\nPrice paid: {price:.2f} $SHK")
-        self.webhook.send(f"\U0001F916 New {mint_item} Alert!\n\tMinted Price: `{price:.2f} $SHK`")
+        self.webhook.send(
+            f"\U0001F916 New {mint_item} Mint Alert!\n\tMinted Price: `{price:.2f} $SHK`"
+        )
 
     async def event_monitors(self, interval: float) -> None:
         while True:
@@ -95,7 +165,16 @@ class MechMonitor:
             message += f"**SHK Deposited**: `{shk_balance:.2f}`\n"
             message += f"**Multiplier**: `{multiplier:.2f}`\n"
             message += f"**Current Mint Price**: `{min_mint_shk:.2f} $SHK`\n"
-            message += f"**SHK Minted Mechs**: `{num_minted_mechs_from_shk:.2f}`\n"
+            message += f"**SHK Minted Mechs**: `{num_minted_mechs_from_shk:.2f}`\n\n"
+            message += f"**Guild Distribution**\n"
+            holders = await self.snowtrace_api.get_erc721_token_transfers(self.address)
+            for address, data in holders.items():
+                address = Web3.toChecksumAddress(address)
+                owner = self.address_mapping.get(address, address)
+                text = f"\t{owner}:"
+                for nft_type, tokens in data.items():
+                    text += f" {nft_type}s: {len(tokens)}"
+                message += text + "\n"
             logger.print_ok_blue(message)
             self.webhook.send(message)
             await asyncio.sleep(interval)
