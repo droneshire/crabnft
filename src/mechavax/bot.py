f8c320addcf8123eb61c0116dff4e997e7d06d00
Ross Yeager <ryeager12@gmail.com>
2023-01-31 23:48:38 -0800

    wip

--- src/mechavax/bot.py
+++ src/mechavax/bot.py
@@ -227,7 +227,10 @@ class MechBot:
 
             if savings_margin < savings_percent:
                 logger.print_normal(
-                    f"Skipping minting since we don't have enough SHK: Have {shk_balance:.2f}, Need {min_mint_shk * (1 + savings_percent):.2f}"
+                    f"Skipping minting since we don't have enough SHK: {savings_margin * 100.0:.2f}%"
+                )
+                logger.print_normal(
+                    f"Have {shk_balance:.2f}, Need {min_mint_shk * (1 + savings_percent):.2f}"
                 )
                 await asyncio.sleep(self.MINT_BOT_INTERVAL)
                 continue
