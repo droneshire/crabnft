577d5ca5952d1e17afb781e0ebca5fe3c0b09267
Ross Yeager <ryeager12@gmail.com>
2023-02-02 22:19:05 -0800

    wip

--- src/web3_utils/snowtrace.py
+++ src/web3_utils/snowtrace.py
@@ -2,7 +2,8 @@ import typing as T
 
 from eth_typing import Address
 
-from utils import logger
+from config_admin import SNOWTRACE_API_KEY
+from utils import logger, price
 from utils.web2_client import Web2Client
 
 
@@ -36,6 +37,35 @@ class SnowtraceApi(Web2Client):
                 tokens[address][nft_type] = tokens[address].get(nft_type, []) + [token_id]
                 logger.print_normal(f"Found {nft_type} {token_id} from {address}")
         except:
-            logger.print_fail(f"Failed to get token transfers")
+            logger.print_fail(f"Failed to get ERC721 token transfers")
 
+        return tokens
+
+    def get_erc20_token_transfers(
+        self, target_address: Address
+    ) -> T.Dict[Address, T.Dict[str, float]]:
+        url = self.SNOWTRACE_API_URL
+        params = {
+            "module": "account",
+            "action": "tokentx",
+            "address": target_address,
+            "startblock": 0,
+            "endblock": 999999999,
+            "sort": "asc",
+            "apikey": SNOWTRACE_API_KEY,
+        }
+
+        tokens = {}
+        try:
+            response = self._get_request(url, params=params)
+            for transfer in response["result"]:
+                address = transfer["from"]
+                symbol = transfer["tokenSymbol"]
+                amount_wei = transfer["value"]
+                amount = price.wei_to_token(amount_wei)
+                if address not in tokens:
+                    tokens[address] = {}
+                tokens[address][symbol] = tokens[address].get(symbol, 0) + amount
+        except:
+            logger.print_fail(f"Failed to get ERC20 token transfers")
         return tokens
