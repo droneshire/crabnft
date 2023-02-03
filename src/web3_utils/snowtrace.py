c718c6c99ca2b09f4c90712867dc662d53c941db
Ross Yeager <ryeager12@gmail.com>
2023-02-02 22:50:17 -0800

    wip

--- src/web3_utils/snowtrace.py
+++ src/web3_utils/snowtrace.py
@@ -1,6 +1,8 @@
+import json
 import typing as T
 
 from eth_typing import Address
+from web3 import Web3
 
 from config_admin import SNOWTRACE_API_KEY
 from utils import logger, price
@@ -30,12 +32,15 @@ class SnowtraceApi(Web2Client):
             response = self._get_request(url, params=params)
             for token in response["result"]:
                 address = token["from"]
+                checksum_address = Web3.toChecksumAddress(address)
                 token_id = token["tokenID"]
                 nft_type = token["tokenSymbol"]
-                if address not in tokens:
-                    tokens[address] = {}
-                tokens[address][nft_type] = tokens[address].get(nft_type, []) + [token_id]
-                logger.print_normal(f"Found {nft_type} {token_id} from {address}")
+                if checksum_address not in tokens:
+                    tokens[checksum_address] = {}
+                tokens[checksum_address][nft_type] = tokens[checksum_address].get(nft_type, []) + [
+                    token_id
+                ]
+                logger.print_normal(f"Found {nft_type} {token_id} from {checksum_address}")
         except:
             logger.print_fail(f"Failed to get ERC721 token transfers")
 
@@ -60,12 +65,17 @@ class SnowtraceApi(Web2Client):
             response = self._get_request(url, params=params)
             for transfer in response["result"]:
                 address = transfer["from"]
+                to_address = transfer["to"]
+                checksum_address = Web3.toChecksumAddress(address)
+                if Web3.toChecksumAddress(to_address) != Web3.toChecksumAddress(target_address):
+                    continue
                 symbol = transfer["tokenSymbol"]
-                amount_wei = transfer["value"]
+                amount_wei = int(transfer["value"])
                 amount = price.wei_to_token(amount_wei)
-                if address not in tokens:
-                    tokens[address] = {}
-                tokens[address][symbol] = tokens[address].get(symbol, 0) + amount
+                if checksum_address not in tokens:
+                    tokens[checksum_address] = {}
+                tokens[checksum_address][symbol] = tokens[checksum_address].get(symbol, 0) + amount
+                logger.print_normal(f"Found {amount:.2f} {symbol} from {checksum_address}")
         except:
             logger.print_fail(f"Failed to get ERC20 token transfers")
         return tokens
